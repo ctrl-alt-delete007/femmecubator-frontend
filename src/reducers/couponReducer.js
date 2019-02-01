477f58a8f3c77fdff489059b9d66728ecdf13d41
cfernando415 <ca.fernando415@gmail.com>
2019-01-31 22:25:39 -0500

    setting up redux

---
+++ src/reducers/couponReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  coupons: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
