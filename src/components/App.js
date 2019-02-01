2c9103ab4bbd1ba191aefb955438cf44de5a5510
cfernando415 <ca.fernando415@gmail.com>
2019-01-31 23:06:04 -0500

    hide api key

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
+console.log("api key", process.env.REACT_APP_API_KEY_MEETUP);
 class App extends Component {
   render() {
     return <div className="App" />;
