09956a75bc5cdf5097ff586e577e5f3a8a0fca04
cfernando415 <ca.fernando415@gmail.com>
2019-02-02 23:20:08 -0500

    login user: wip

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -2,3 +2,8 @@ export const registerUser = userInfo => ({
   type: "REGISTER_USER",
   payload: userInfo
 });
+
+export const loginUser = userInfo => ({
+  type: "LOGIN_USER",
+  payload: userInfo
+});
