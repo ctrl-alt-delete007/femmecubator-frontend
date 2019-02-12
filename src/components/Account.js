0ee933aebe2c632e5d5bb36f226cde88bf3d5eef
cfernando415 <ca.fernando415@gmail.com>
2019-02-12 18:43:16 -0500

    account:wip

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,11 +6,42 @@ class Account extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      tab1: true,
+      tab2: false
+    };
+
     this.props.getCurrentUserFromStore();
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
   render() {
-    return <Fragment>Do Something</Fragment>;
+    return (
+      <Fragment>
+        <div
+          onClick={this.clickHandler}
+          className="ui top attached tabular menu"
+        >
+          <a name="t1" className={this.state.tab1 ? "item active" : "item"}>
+            Tab 1
+          </a>
+          <a name="t2" className={this.state.tab2 ? "item active" : "item"}>
+            Tab 2
+          </a>
+        </div>
+        <div className="ui bottom attached segment">
+          {this.state.tab1 ? "tab1" : "tab2"}
+        </div>
+      </Fragment>
+    );
+  }
+
+  clickHandler(e) {
+    if (e.target.name === "t1") {
+      this.setState({ tab1: true, tab2: false });
+    } else if (e.target.name === "t2") {
+      this.setState({ tab1: false, tab2: true });
+    }
   }
 }
 
