385f163f57f69cae9bb7cf78a412b57b2fc0188a
cfernando415 <ca.fernando415@gmail.com>
2019-02-12 00:09:34 -0500

    events css:wip

--- src/components/meetup.js
+++ src/components/meetup.js
@@ -9,8 +9,17 @@ class Meetup extends Component {
           <a href={meetup.url}>{meetup.name}</a>
         </td>
         <td>{meetup.sponsor}</td>
-        <td>{meetup.local_date}</td>
-        <td>{meetup.local_time}</td>
+        <td>{new Date(meetup.event_date).toLocaleDateString("en-US")}</td>
+        <td>
+          {new Date(meetup.event_date + " " + meetup.event_time).toLocaleString(
+            "en-US",
+            {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true
+            }
+          )}
+        </td>
         <td>{meetup.location}</td>
       </tr>
     );
