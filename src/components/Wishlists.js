07a9f15470501ec39d5f9d54e2205bff4cf41e51
cfernando415 <ca.fernando415@gmail.com>
2019-02-12 21:22:33 -0500

    wishlist:done

--- src/components/Wishlists.js
+++ src/components/Wishlists.js
@@ -1,16 +1,24 @@
 import React, { Component, Fragment } from "react";
 import WishlistItem from "./WishlistItem";
+import { fetchCurrentUser } from "../thunks/accountThunks";
+import { connect } from "react-redux";
+
 class Wishlists extends Component {
   constructor(props) {
     super(props);
     this.state = {
       wishlists: []
     };
+
+    this.props.fetchCurrentUser();
   }
 
   render() {
-    const tmp = this.props.wishlists || [{}];
-    const wishlists = tmp.map((wishlistItem, i) => (
+    const currentUser = this.props.currentUser.membershipInfo || {
+      coupons: []
+    };
+
+    const wishlists = currentUser.coupons.map((wishlistItem, i) => (
       <WishlistItem key={i} wishlistItem={wishlistItem} />
     ));
 
@@ -31,4 +39,15 @@ class Wishlists extends Component {
   }
 }
 
-export default Wishlists;
+const mapStateToProps = state => {
+  return { currentUser: state.accountInfo.currentUser };
+};
+
+const mapDispatchToProps = dispatch => {
+  return { fetchCurrentUser: () => dispatch(fetchCurrentUser()) };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Wishlists);
