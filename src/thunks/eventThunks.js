527e7d23ebcdda569d4480be210ba8a2fe9c5e18
cfernando415 <ca.fernando415@gmail.com>
2019-02-23 16:15:36 -0500

    hide api key

--- src/thunks/eventThunks.js
+++ src/thunks/eventThunks.js
@@ -2,9 +2,7 @@ import { loadMeetups } from "../actions/eventActions";
 
 export const getMeetups = () => dispatch => {
   return fetch(
-    `https://cors-anywhere.herokuapp.com/http://api.meetup.com/find/upcoming_events?key=${
-      process.env.MEETUP_API_KEY
-    }&sign=true&format=json&photo-host=public&topic_category=witi`,
+    `https://cors-anywhere.herokuapp.com/http://api.meetup.com/find/upcoming_events?key=13a6c19667bd255232452d766c2f6c&sign=true&format=json&photo-host=public&topic_category=witi`,
     { mode: "cors" }
   )
     .then(resp => resp.json())
